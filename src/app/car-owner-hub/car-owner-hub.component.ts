6e0ed6fa5727da7eac18bf281559e6d648eb26c2
EgeKaya <egecankaya@hotmail.com>
2018-07-26 13:39:41 -0400

    Minor changes.

--- src/app/car-owner-hub/car-owner-hub.component.ts
+++ src/app/car-owner-hub/car-owner-hub.component.ts
@@ -7,23 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./car-owner-hub.component.css']
 })
 export class CarOwnerHubComponent implements OnInit {
+  // Owner ID and Owner Prefix. Used when making calls to the API.
   ownerID : string = "1234";
   ownerPrefix: string = "resource:org.example.scottpoc.carOwner#";
+
+  // Table which will be populated with the data fetched from the API.
   tableData =[];
+
+  // Owner name which will be fetched from the API.
   ownerName: string;
+
+
   constructor(private restService: RestService) { }
 
   ngOnInit() {
-    this.restService.isWorking();
-    this.restService.getAllFrom("vehicle").subscribe(data=>{
-      data.forEach(vehic =>{
-        if(vehic.owner == this.ownerPrefix + this.ownerID)
-        {
-          this.tableData.push(vehic);
-        }
-      })
-
-    });
+    // Make a call to the API to fetch user information.
     this.restService.getAllFrom("carOwner").subscribe(data=>{
       data.forEach(person =>{
         if(person.ownerId == this.ownerID)
@@ -31,7 +29,16 @@ export class CarOwnerHubComponent implements OnInit {
           this.ownerName = person.firstName + " "+ person.lastName;
         }
       })
-      
+    });
+
+    // Make a call to the API to fetch all vehicles registered for this user.
+    this.restService.getAllFrom("vehicle").subscribe(data=>{
+      data.forEach(vehic =>{
+        if(vehic.owner == this.ownerPrefix + this.ownerID)
+        {
+          this.tableData.push(vehic);
+        }
+      })
     });
   }
 }
