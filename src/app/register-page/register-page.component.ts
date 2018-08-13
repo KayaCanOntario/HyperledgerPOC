13730f2c3e6f09d7220d40f56c5e13cbd75dc468
Scott A. Riva <scottariva@gmail.com>
2018-08-13 13:50:09 -0400

    shortening user IDs

--- src/app/register-page/register-page.component.ts
+++ src/app/register-page/register-page.component.ts
@@ -39,7 +39,7 @@ export class RegisterPageComponent implements OnInit {
           this.newCarOwner.firstName = this.inFirstName;
           this.newCarOwner.lastName = this.inLastName;
           this.newCarOwner.pass = this.inPassword;
-          this.newCarOwner.ownerId = (Math.random()*100000000000000000).toString();
+          this.newCarOwner.ownerId = (Math.round(Math.random()*100000)).toString();
           this.restService.postTo("carOwner", this.newCarOwner).subscribe(data=>{
             window.localStorage[0] = this.inEmail;
             this.router.navigate(['/car-owner']);
@@ -50,7 +50,7 @@ export class RegisterPageComponent implements OnInit {
           this.newManufacturer.email = this.inEmail;
           this.newManufacturer.name = this.inFirstName;
           this.newManufacturer.pass = this.inPassword;
-          this.newManufacturer.manId = (Math.random()*100000000000000000).toString();
+          this.newManufacturer.manId = (Math.round(Math.random()*100000)).toString();
           this.restService.postTo("manufacturer", this.newManufacturer).subscribe(data=>{
             window.localStorage[0] = this.inEmail;
             this.router.navigate(['/manufacturer']);
@@ -62,7 +62,7 @@ export class RegisterPageComponent implements OnInit {
           this.newPolice.firstName = this.inFirstName;
           this.newPolice.lastName = this.inLastName;
           this.newPolice.pass = this.inPassword;
-          this.newPolice.badgeNumber = (Math.random()*100000000000000000).toString();
+          this.newPolice.badgeNumber = (Math.round(Math.random()*100000)).toString();
           this.restService.postTo("policeOfficer", this.newPolice).subscribe(data=>{
             window.localStorage[0] = this.inEmail;
             this.router.navigate(['/police']);
